2b526793f47bbc79138b8cc6e21a65a23aac7d1f
Ehevi <48785655+Ehevi@users.noreply.github.com>
2020-01-25 13:27:52 +0100

    course ratings

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Courses } from '../Courses';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: []
+  styleUrls: ['../kurs/kurs.component.css']
 })
 export class ListaComponent implements OnInit {
 
+  @Input() public course: Courses;
+
+  courseRated: boolean;
+  rating: number;
   constructor() { }
 
   ngOnInit() {
+    this.courseRated = false;
   }
 
+  onRate(rating) {
+    if (rating.value !== 0) {
+      this.rating = rating.value;
+      this.courseRated = true;
+    }
+  }
 }
