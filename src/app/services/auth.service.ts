b2eba5069afdf1cbe5d17f4e016f75a616473f78
unknown <aga.cyra@wp.pl>
2020-01-28 09:44:12 +0100

    imports in auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Permissions } from '../types/permissions';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../types/user';
+import { auth } from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +15,7 @@ export class AuthService {
   private permissions: BehaviorSubject<Permissions>;
 
   constructor() {
-    this.permissions = new BehaviorSubject(Permissions.STUDENT);
+    this.permissions = new BehaviorSubject(Permissions.LOGGED_OUT);
   }
 
   getPermissions(): Observable<Permissions> {
