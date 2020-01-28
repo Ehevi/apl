2826406752af62b7889be44d157335b75c218251
unknown <aga.cyra@wp.pl>
2020-01-28 11:44:41 +0100

    authservice functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,13 +13,76 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   private permissions: BehaviorSubject<Permissions>;
+  userData: any;
 
-  constructor() {
+  constructor(
+    public afs: AngularFirestore,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public ngZone: NgZone
+  ) {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user'));
+      } else {
+        localStorage.setItem('user', null);
+        JSON.parse(localStorage.getItem('user'));
+      }
+    });
     this.permissions = new BehaviorSubject(Permissions.LOGGED_OUT);
   }
 
   getPermissions(): Observable<Permissions> {
     return this.permissions;
+  }
+
+   LogIn(email, password) {
+    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['dashboard']);
+        });
+        this.SetUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  Register(email, password) {
+    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.SetUserData(result.user);
+      }).catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user'));
+    return (user !== null);
+  }
+
+  SetUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      semester: user.semester,
+      typeofuser: user.typeofuser,
+    }
+    return userRef.set(userData, {
+      merge: true
+    });
+  }
+
+  LogOut() {
+    return this.afAuth.auth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['sign-in']);
+    });
   }
 
 }
--- src/app/types/user.ts
+++ src/app/types/user.ts
@@ -3,4 +3,5 @@ export interface User {
     email: string;
     displayName: string;
     semester: number;
+    typeofuser: Permissions;
 }
